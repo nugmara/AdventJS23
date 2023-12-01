4e42f6a0acf89a8ae98ce6615be3d2af612a037b
nugmara <dagmarag0@gmail.com>
2023-12-01 21:17:45 +0100

    First problem solved without comments

--- Day-1/script.js
+++ Day-1/script.js
@@ -1,1 +1,20 @@
 //  FIRST GIFT REPEATED
+
+function findFirstRepeated(gifts) {
+    const indexMap = {};
+    let minIndex = gifts.length;    
+    let result = -1;
+
+    for (let i = 0; i < gifts.length; i++) {
+        const gift = gifts[i];
+        if (indexMap[gift] === undefined) {
+            indexMap[gift] = i;
+        } else {
+            if (indexMap[gift] < minIndex) {
+                minIndex = i;
+                result = gift;
+            }
+        }
+    }
+    return result;
+}
