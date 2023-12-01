9bde2fc6cce0c93802601c718dc37bcacc66f54b
nugmara <dagmarag0@gmail.com>
2023-12-01 23:13:26 +0100

    Add comments to our code

--- Day-1/script.js
+++ Day-1/script.js
@@ -1,12 +1,25 @@
 //  FIRST GIFT REPEATED
 
+/* 
+    INITIALIZATION
+    -indexMap: empty object to store the index of the first occurrence of each gift (number) in the array
+    -minIndex: the index of the first repeated gift in the array (initialized to the last index of the array). It will be used to keep track of the minimum index of the second occurrence of numbers.
+    -result: initialized to -1
+*/ 
 function findFirstRepeated(gifts) {
-    const indexMap = {};
+    const indexMap = {}; 
     let minIndex = gifts.length;    
     let result = -1;
 
+    /* 
+        LOOP THROUGH THE ARRAY
+        -if the gift is not in the indexMap, add it to the indexMap with the index as the value
+        -if the gift is already in the indexMap, check if the index is smaller than the minIndex
+            -if it is, update the minIndex and result
+    */
     for (let i = 0; i < gifts.length; i++) {
-        const gift = gifts[i];
+        const gift = gifts[i]; // the current gift in the array
+
         if (indexMap[gift] === undefined) {
             indexMap[gift] = i;
         } else {
