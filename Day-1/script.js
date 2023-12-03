fea5e7afdb500d9c710d70efff2f775213ef954b
nugmara <dagmarag0@gmail.com>
2023-12-03 16:55:14 +0100

    Add explanation comments of the problem and comments of my code

--- Day-1/script.js
+++ Day-1/script.js
@@ -1,3 +1,4 @@
+// Challenge #1
 //  FIRST GIFT REPEATED
 
 /* 
--- Day-2/script.js
+++ Day-2/script.js
@@ -1,3 +1,14 @@
+// Challenge #2
+// We start a factory
+
+/* 
+In Santa's workshop, the elves have a gift list they wish to make and a limited set of materials.
+
+Gifts are strings of text and materials are characters. Your task is to write a function that, given a list of gifts and the available materials, returns a list of the gifts that can be made.
+
+A gift can be made if we have all the necessary materials to make it.
+*/
+
 function manufacture(gifts, materials) {
     const manufacturedGifts = [];
     
@@ -6,6 +17,10 @@ function manufacture(gifts, materials) {
       
       for (let j = 0; j < materials.length; j++) {
         // Check if the current material is present in the gift
+
+        //! At first I put an if statement here to check if the gift includes the material, but it didn't work. Why?
+        //! Instead we should use a while loop to check if the gift includes the material, because we want to remove all the instances of the material from the gift.
+        //! If we use an if statement, it will only remove the first instance of the material from the gift.
         while (gift.includes(materials[j])) {
         // If yes, remove it from the gift
           gift = gift.replace(materials[j], "")
