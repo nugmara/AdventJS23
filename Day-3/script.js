2b063a155ccf32c822e616e3374a151c4ac131ee
nugmara <dagmarag0@gmail.com>
2023-12-05 21:05:24 +0100

    Add problem description

--- Day-3/script.js
+++ Day-3/script.js
@@ -1,2 +1,10 @@
 // Challenge #3
 // The naughty elf
+
+/* 
+In Santa's workshop, a mischievous elf has been playing around with the gift production line, adding or removing an unplanned step.
+
+You have the original sequence of original manufacturing steps and the modified modified sequence that may include an extra step or be missing a step.
+
+Your task is to write a function that identifies and returns the first extra step that was added or removed in the manufacturing chain. If there is no difference between the sequences, return an empty string.
+*/
