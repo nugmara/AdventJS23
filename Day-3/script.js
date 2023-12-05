6c8301e2d6309902fac46b71c6cf1bd85b349a8b
nugmara <dagmarag0@gmail.com>
2023-12-05 21:14:28 +0100

    Add solution and my test failed

--- Day-3/script.js
+++ Day-3/script.js
@@ -8,3 +8,29 @@ You have the original sequence of original manufacturing steps and the modified 
 
 Your task is to write a function that identifies and returns the first extra step that was added or removed in the manufacturing chain. If there is no difference between the sequences, return an empty string.
 */
+
+
+function findNaughtyStep(original, modified) {
+    let result = "";
+ 
+   for (let i = 0; i < modified.length; i++) {
+     if (original[i] !== modified[i]) {
+       return modified[i]
+     }
+   }
+   return result;
+ }
+
+ //! I have just one text failed AND I DON'T KNOW WHY
+ // This is the test failed:
+ // 
+
+ /*  
+ Test: findNaughtyStep('stepfor', 'stepor')
+
+Expected:
+"f"
+
+Actual:
+"o"
+ */ 
